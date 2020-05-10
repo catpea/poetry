3ddfbf07288a6fe129e6e0b605265321358bbe83
Cat Pea <corporate.miou@gmail.com>
2020-05-10 09:02:13 -0400

    System Update 20/65

--- bin/publish/index.sh
+++ bin/publish/index.sh
@@ -4,5 +4,6 @@ year=$(date +"%y");
 poem=$(ls -1 db/*.md | wc -l);
 
 git add .;
-git commit -m "New Release: RC.$year.$poem";
+git commit -m "System Update $year/$poem";
+npm version minor; # this does add and commit too.
 git push;
